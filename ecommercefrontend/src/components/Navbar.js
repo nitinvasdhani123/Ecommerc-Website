88711063cbc2fec08845180e170cded0b4296aa8
pranjalcool9897 <pranjalkulshresth9897@gmail.com>
2023-08-16 00:31:30 +0530

    navbar for medium screen

--- ecommercefrontend/src/components/Navbar.js
+++ ecommercefrontend/src/components/Navbar.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Menu from './Menu'
+
 
 function Navbar() {
+  const user=false;
   return (
-    <div>
-      <ul>
-      <li className='text-6xl'><Link to="/Home">Home</Link></li>
-        <li><Link to="/Products">Products</Link></li>
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/Cart">Cart</Link></li>
-        <li><Link to="/Dashboard">Dashboard</Link></li>
-      </ul>
+    
+    <>
+    <div className=' flex font-medium justify-between h-12 p-4 items-center border-b-2 border-b-red-500 uppercase'>
+      <div className='hidden md:flex gap-4'>
+        <Link to="/Home">Home</Link>
+       
+        <Link to="/About">About</Link>
+        <Link to="/Dashboard">Dashboard</Link>
+        <Link to="/Cart">Cart</Link>
+      </div>
+      <nav className='text-xl text-red-500 font-bold'>
+        <Link href='/'>Ecommerce</Link>
+      </nav>
+
+      <div className='md:hidden'><Menu/></div>
+      <div className=' md:flex hidden gap-5'>
+      {!user?<Link href='/'>Login</Link>:
+       <Link to="/Products">Products</Link>}
+      
+      </div>
     </div>
+    </>
   )
 }
 
