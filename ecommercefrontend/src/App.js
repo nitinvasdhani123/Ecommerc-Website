f4f1dbd16d72a9578e1b9d76697097977523c815
Nitin Kumar Vasdhani <nitinkumarvasdhani786@gmail.com>
2023-08-15 21:30:30 +0530

    first commit

---
+++ ecommercefrontend/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Home from './components/Home'
+import Products from './components/Products'
+import About from './components/About'
+import Cart from './components/Cart'
+import Dashboard from './components/Dashboard'
+import Main from './components/Main'
+import { BrowserRouter as Router, Route , Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+       <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/Signup" element={<Signup />} />
+          <Route exact path="/Main" element={<Main />} />
+          <Route exact path="/Home" element={<Home />} />
+          <Route exact path="/Products" element={<Products />} />
+          <Route exact path="/About" element={<About />} />
+          <Route exact path="/Cart" element={<Cart />} />
+          <Route exact path="/Dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ ecommercefrontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+      <li><Link to="/Home">Home</Link></li>
+        <li><Link to="/Products">Products</Link></li>
+        <li><Link to="/About">About</Link></li>
+        <li><Link to="/Cart">Cart</Link></li>
+        <li><Link to="/Dashboard">Dashboard</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
